b7deb76cdcea8b6918323cffef235e946d100d71
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-24 01:39:09 +0900

    add :: Meeting 엔티티 추가

---
+++ src/main/java/com/tomato/running/domain/meeting/Meeting.java
@@ -0,0 +1,42 @@
+package com.tomato.running.domain.meeting;
+
+import com.tomato.running.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Meeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer distance;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String startedLocation;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String endedLocation;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
