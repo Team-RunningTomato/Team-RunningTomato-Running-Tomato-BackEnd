5d1e0f046fe64abea171ed9497f30b42488e399e
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-24 01:39:18 +0900

    add :: MeetingMember 엔티티 추가

---
+++ src/main/java/com/tomato/running/domain/meeting/MeetingMember.java
@@ -0,0 +1,25 @@
+package com.tomato.running.domain.meeting;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MeetingMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "meeting_id", nullable = false)
+    private Meeting meeting;
+}
