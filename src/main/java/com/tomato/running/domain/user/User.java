83198e1e073a8f6e810e927f98b215dd93bbe8dd
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-24 01:39:32 +0900

    add :: User 엔티티 추가

---
+++ src/main/java/com/tomato/running/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.tomato.running.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(length = 10, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String nickname;
+
+    @Column(length = 10, nullable = false)
+    private Integer height;
+
+    @Column(length = 10, nullable = false)
+    private Integer weight;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String area;
+}
