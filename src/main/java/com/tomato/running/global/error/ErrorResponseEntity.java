553e7e83a3b46998d38405a8b5da7383efc37a80
yena5511 <ar01077422264@gmail.com>
2024-11-06 09:52:57 +0900

    Revert "refactor :: ResponseEntity record 사용"
    This reverts commit f30ed2c797fad6733879747243b405d8934745f3.

--- src/main/java/com/tomato/running/global/error/ErrorResponseEntity.java
+++ src/main/java/com/tomato/running/global/error/ErrorResponseEntity.java
@@ -1,15 +1,24 @@
 package com.tomato.running.global.error;
+
+
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
 
-public record ErrorResponseEntity(int status, String name, String message) {
+@Data
+@Builder
+public class ErrorResponseEntity {
+    private int status;
+    private String name;
+    private String message;
 
-    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode e) {
+    public static ResponseEntity<ErrorResponseEntity> toResponseEntity (ErrorCode e) {
         return ResponseEntity
                 .status(e.getHttpStatus())
-                .body(new ErrorResponseEntity(
-                        e.getHttpStatus().value(),
-                        e.name(),
-                        e.getMessage()
-                ));
+                .body(ErrorResponseEntity.builder()
+                        .status(e.getHttpStatus().value())
+                        .name(e.name())
+                        .message(e.getMessage())
+                        .build());
     }
 }
