dd89c184fb7b59bf2159e592f22dee4db9252af5
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-26 17:21:42 +0900

    add :: 토큰 재발급 api 추가

---
+++ src/main/java/com/tomato/running/domain/auth/service/ReissueTokenService.java
@@ -0,0 +1,42 @@
+package com.tomato.running.domain.auth.service;
+
+import com.tomato.running.domain.auth.exception.TokenNotFoundException;
+import com.tomato.running.domain.auth.presentation.data.res.TokenDto;
+import com.tomato.running.domain.auth.repository.RefreshTokenRepository;
+import com.tomato.running.global.annotation.TransactionService;
+import com.tomato.running.global.security.jwt.TokenProvider;
+import com.tomato.running.global.security.util.count.RefreshToken;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@TransactionService
+@RequiredArgsConstructor
+public class ReissueTokenService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public TokenDto reissueToken(String refreshToken) {
+        String parseRefreshToken = tokenProvider.parseRefreshToken(refreshToken);
+
+        RefreshToken findRefreshToken = refreshTokenRepository.findByToken(parseRefreshToken)
+                .orElseThrow(TokenNotFoundException::new);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(findRefreshToken.getId());
+
+        saveFreshToken(tokenDto.getRefreshToken(), findRefreshToken.getId());
+
+        return tokenDto;
+    }
+
+    private void saveFreshToken(String token, UUID id) {
+        RefreshToken refreshToken = RefreshToken.builder()
+                .id(id)
+                .token(token)
+                .build();
+
+        refreshTokenRepository.save(refreshToken);
+    }
+
+}
