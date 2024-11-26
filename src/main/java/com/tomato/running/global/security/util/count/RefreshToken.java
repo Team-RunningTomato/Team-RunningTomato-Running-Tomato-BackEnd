7480f383fbc5107087ad3b86e46325d147a48984
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-26 12:05:32 +0900

    add :: RefreshToken 추가

---
+++ src/main/java/com/tomato/running/global/security/util/count/RefreshToken.java
@@ -0,0 +1,22 @@
+package com.tomato.running.global.security.util.count;
+
+import com.tomato.running.global.security.jwt.TokenProvider;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.util.UUID;
+
+@RedisHash(value = "expo_refreshToken", timeToLive = TokenProvider.REFRESH_TOKEN_EXPIRE_TIME)
+@Builder
+@Getter
+public class RefreshToken {
+
+    @Id
+    private UUID id;
+
+    @Indexed
+    private String token;
+}
