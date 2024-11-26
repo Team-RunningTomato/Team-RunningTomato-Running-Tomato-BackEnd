e4c03c6d2138db0ac6fbfe898985377c3a71718c
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-26 17:21:53 +0900

    add :: findByToken 메서드 추가

--- src/main/java/com/tomato/running/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/tomato/running/domain/auth/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.tomato.running.domain.auth.repository;
 import com.tomato.running.global.security.util.count.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
