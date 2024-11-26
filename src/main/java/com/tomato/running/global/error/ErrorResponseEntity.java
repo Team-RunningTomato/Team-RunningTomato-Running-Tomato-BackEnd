1027923b4f8b10724acc4a2401552dea37457cca
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-11-26 17:35:37 +0900

    update :: ErrorResponseEntity 변경

--- src/main/java/com/tomato/running/global/error/ErrorResponseEntity.java
+++ src/main/java/com/tomato/running/global/error/ErrorResponseEntity.java
@@ -1,14 +1,13 @@
 package com.tomato.running.global.error;
 import org.springframework.http.ResponseEntity;
 
-public record ErrorResponseEntity(int status, String name, String message) {
+public record ErrorResponseEntity(int status, String message) {
 
     public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode e) {
         return ResponseEntity
-                .status(e.getHttpStatus())
+                .status(e.getStatus())
                 .body(new ErrorResponseEntity(
-                        e.getHttpStatus().value(),
-                        e.name(),
+                        e.getStatus(),
                         e.getMessage()
                 ));
     }
