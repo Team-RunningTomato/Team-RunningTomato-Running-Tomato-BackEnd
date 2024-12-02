fb4c878aea67015491fecaceab1f9472928f4269
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-02 14:11:04 +0900

    add :: Logout api 추가

---
+++ src/main/java/com/tomato/running/domain/auth/service/LogoutService.java
@@ -0,0 +1,20 @@
+package com.tomato.running.domain.auth.service;
+
+import com.tomato.running.domain.auth.repository.RefreshTokenRepository;
+import com.tomato.running.domain.user.User;
+import com.tomato.running.domain.user.util.UserUtil;
+import com.tomato.running.global.annotation.ReadOnlyTransactionService;
+import lombok.RequiredArgsConstructor;
+
+@ReadOnlyTransactionService
+@RequiredArgsConstructor
+public class LogoutService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserUtil userUtil;
+
+    public void logout() {
+        User user = userUtil.getCurrentUser();
+        refreshTokenRepository.deleteById(user.getId());
+    }
+}
