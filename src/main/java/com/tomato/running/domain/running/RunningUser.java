c5ca8c293ea37b9789f05e0af60c9ab88a885cd2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-17 13:32:59 +0900

    add :: RunningUser 추가

---
+++ src/main/java/com/tomato/running/domain/running/RunningUser.java
@@ -0,0 +1,38 @@
+package com.tomato.running.domain.running;
+
+import com.tomato.running.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RunningUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, columnDefinition = "BIGINT(10)")
+    private Long totalDistance;
+
+    @Column(nullable = false, columnDefinition = "BIGINT(10)")
+    private Long bestDistance;
+
+    @Column(nullable = false, columnDefinition = "BIGINT(10)")
+    private Long worstDistance;
+
+    @Column(nullable = false)
+    private Integer levelPercentage;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
