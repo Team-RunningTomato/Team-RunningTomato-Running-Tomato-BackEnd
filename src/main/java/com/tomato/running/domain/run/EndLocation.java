2542d7f5e34b60586389902a9a719d404edaebf3
yena5511 <ar01077422264@gmail.com>
2024-12-17 19:50:07 +0900

    add :: @AllArgsConstructor 추가

--- src/main/java/com/tomato/running/domain/run/EndLocation.java
+++ src/main/java/com/tomato/running/domain/run/EndLocation.java
@@ -2,10 +2,12 @@ package com.tomato.running.domain.run;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class EndLocation {
--- src/main/java/com/tomato/running/domain/run/StartLocation.java
+++ src/main/java/com/tomato/running/domain/run/StartLocation.java
@@ -2,11 +2,13 @@ package com.tomato.running.domain.run;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class StartLocation {
 
