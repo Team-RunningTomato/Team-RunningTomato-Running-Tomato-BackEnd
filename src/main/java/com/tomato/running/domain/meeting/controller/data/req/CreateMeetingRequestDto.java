87c7a6a829144013da6147b065f4cee45189c44b
yena5511 <ar01077422264@gmail.com>
2024-12-17 19:48:55 +0900

    add :: CreateMeetingRequestDto 추가

---
+++ src/main/java/com/tomato/running/domain/meeting/controller/data/req/CreateMeetingRequestDto.java
@@ -0,0 +1,35 @@
+package com.tomato.running.domain.meeting.controller.data.req;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class CreateMeetingRequestDto {
+    @NotNull
+    private String title;
+
+    @NotNull
+    private Integer distance;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @NotNull
+    private LocalDateTime startAt;
+
+    @NotNull
+    private Float startLongitude;
+
+    @NotNull
+    private Float startLatitude;
+
+    @NotNull
+    private Float endLongitude;
+
+    @NotNull
+    private Float endLatitude;
+
+}
