7fe769624fd654d976a44bfcd1b92c6bb9f89118
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-23 23:17:34 +0900

    update :: 접근제어자 추가

--- src/main/java/com/tomato/running/global/error/CustomException.java
+++ src/main/java/com/tomato/running/global/error/CustomException.java
@@ -1,10 +1,10 @@
 package com.tomato.running.global.error;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException{
-    ErrorCode errorCode;
+    private final ErrorCode errorCode;
 }
