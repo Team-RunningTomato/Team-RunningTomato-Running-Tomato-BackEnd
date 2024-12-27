3f3d404415c96ed720624011035e8799ee624f94
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-27 10:00:30 +0900

    add :: meetingMember와 Meeting 종료 자동화 메서드 추가

--- src/main/java/com/tomato/running/domain/run/service/RecordRunningService.java
+++ src/main/java/com/tomato/running/domain/run/service/RecordRunningService.java
@@ -1,9 +1,15 @@
 package com.tomato.running.domain.run.service;
 
+import com.tomato.running.domain.meeting.entity.Meeting;
+import com.tomato.running.domain.meeting.entity.MeetingMember;
+import com.tomato.running.domain.meeting.exception.MeetingNotFoundException;
+import com.tomato.running.domain.meeting.repository.MeetingMemberRepository;
+import com.tomato.running.domain.meeting.repository.MeetingRepository;
 import com.tomato.running.domain.run.entity.EndLocation;
 import com.tomato.running.domain.run.entity.Run;
 import com.tomato.running.domain.run.entity.StartLocation;
 import com.tomato.running.domain.run.exception.InvalidRunDistanceException;
+import com.tomato.running.domain.run.exception.MeetingMemberNotFoundException;
 import com.tomato.running.domain.run.presentation.data.req.RecordRunningRequestDto;
 import com.tomato.running.domain.run.repository.RunRepository;
 import com.tomato.running.domain.run.util.LevelUtil;
@@ -14,51 +20,60 @@ import com.tomato.running.domain.user.util.UserUtil;
 import com.tomato.running.global.annotation.TransactionService;
 import lombok.RequiredArgsConstructor;
 
+import java.util.UUID;
+
 @TransactionService
 @RequiredArgsConstructor
 public class RecordRunningService {
 
     private final RunningUserRepository runningUserRepository;
+    private final MeetingRepository meetingRepository;
+    private final MeetingMemberRepository meetingMemberRepository;
     private final LevelUtil levelUtil;
     private final RunRepository runRepository;
     private final UserUtil userUtil;
 
-    public void recordRunning(RecordRunningRequestDto dto) {
+    public void recordRunning(UUID meetingId, RecordRunningRequestDto dto) {
         User user = userUtil.getCurrentUser();
 
-        if (dto.getDistance() < 0)
+        Meeting meeting = meetingRepository.findById(meetingId)
+                .orElseThrow(MeetingNotFoundException::new);
+
+        if (meeting.getDistance() < 0)
             throw new InvalidRunDistanceException();
 
-        saveRun(dto, user);
-        saveRunningUser(user, dto);
+        saveRun(dto, user, meeting);
+        saveRunningUser(user, meeting);
+        processMeetingMember(user, meeting);
     }
 
-    private void saveRun(RecordRunningRequestDto dto, User user) {
+    private void saveRun(RecordRunningRequestDto dto, User user, Meeting meeting) {
         Run run = Run.builder()
-                .distance(dto.getDistance())
+                .distance(meeting.getDistance())
                 .startLocation(new StartLocation(dto.getStartLongitude(), dto.getStartLatitude()))
                 .endLocation(new EndLocation(dto.getEndLongitude(), dto.getEndLatitude()))
                 .runningTime(dto.getRunningTime())
+                .meeting(meeting)
                 .user(user)
                 .build();
 
         runRepository.save(run);
     }
 
-    private void saveRunningUser(User user, RecordRunningRequestDto dto) {
+    private void saveRunningUser(User user, Meeting meeting) {
         RunningUser runningUser = user.getRunningUser();
 
-        Integer best = runningUser.getBestDistance() < dto.getDistance() ? dto.getDistance() : runningUser.getBestDistance();
+        Integer best = runningUser.getBestDistance() < meeting.getDistance() ? meeting.getDistance() : runningUser.getBestDistance();
 
         Integer worst;
 
         if (runningUser.getWorstDistance() == null) {
-            worst = dto.getDistance();
+            worst = meeting.getDistance();
         } else {
-            worst = runningUser.getWorstDistance() > dto.getDistance() ? dto.getDistance() : runningUser.getWorstDistance();
+            worst = runningUser.getWorstDistance() > meeting.getDistance() ? meeting.getDistance() : runningUser.getWorstDistance();
         }
 
-        Integer newTotalDistance = runningUser.getTotalDistance() + dto.getDistance();
+        Integer newTotalDistance = runningUser.getTotalDistance() + meeting.getDistance();
 
         RunningUser newRunningUser = RunningUser.builder()
                 .id(runningUser.getId())
@@ -70,5 +85,16 @@ public class RecordRunningService {
                 .build();
 
         runningUserRepository.save(newRunningUser);
+    }
+
+    private void processMeetingMember(User user, Meeting meeting) {
+        MeetingMember meetingMember = meetingMemberRepository.findByMeetingAndUser(meeting, user)
+                .orElseThrow(MeetingMemberNotFoundException::new);
+
+        meetingMember.updateStatus();
+
+        if (!meetingMemberRepository.existsByMeetingAndStatus(meeting, false)) {
+            meeting.updateStatus();
+        }
     }
 }
