d1cb0af479ab44b430480ce9fdc51e3a7d993d60
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-12-27 10:23:30 +0900

    add :: findByUser 추가

--- src/main/java/com/tomato/running/domain/running/repository/RunningUserRepository.java
+++ src/main/java/com/tomato/running/domain/running/repository/RunningUserRepository.java
@@ -1,7 +1,11 @@
 package com.tomato.running.domain.running.repository;
 
 import com.tomato.running.domain.running.entity.RunningUser;
+import com.tomato.running.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RunningUserRepository extends JpaRepository<RunningUser, Long> {
+    Optional<RunningUser> findByUser(User user);
 }
